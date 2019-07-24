91366fc731a28cd90555d4a2970ab60a9567fc98
irevoire <irevoire@hotmail.fr>
2019-07-24 18:52:43 +0200

    fix easy things

--- src/parser.h
+++ src/parser.h
@@ -20,21 +20,4 @@ static __always_inline int parse_ether(struct hdr_cursor *nh,
 	return 0; // no next header
 }
 
-static __always_inline int parse_ether(struct hdr_cursor *nh,
-                                         void *data_end,
-					 struct ethernet_t **etherhdr)
-{
-	struct ethernet_t *ptr = nh->pos;
-
-	if (ptr + 1 > data_end)
-		return -1;
-
-	nh->pos = ptr + 1;
-
-	if (etherhdr != NULL)
-		*etherhdr = ptr;
-
-	return 0; // no next header
-}
-
 #endif /* _PARSER_H_*/
